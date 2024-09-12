212adddf334fb74541fac248b098c5f009ea6ceb
bibyan06 <vivianprimavera.vivo@bicol-u.edu.ph>
2024-09-13 00:26:26 +0800

    admin side | sept 13

--- resources/views/admin/admin_upload_document.blade.php
+++ resources/views/admin/admin_upload_document.blade.php
@@ -1,123 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-    
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Upload Document</title>
-    <link rel="stylesheet" href="{{ asset ('css/upload_document.css')}}">
-    <link rel="stylesheet" href="{{ asset ('css/admin_page.css') }}">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-</head>
+@extends('layouts.admin_layout')
 
-<body>
-    <header>
-        <div class="header-content">
-            <div class="left-header">
-                <img src="{{ asset('images/Bicol_University.png') }}" alt="Bicol University Logo" class="logo">
-                <h1>BICOL <span>UNIVERSITY</span></h1>
-            </div>
-            <div class="search-container">
-                <input type="text" id="sidebar-search" placeholder="Search">
-                <i class="bi bi-search"></i>
-            </div>
-            <div class="profile-icon">
-                <img src="{{ asset('images/user-circle-solid-24.png') }}" alt="Profile Icon" id="profile-icon">
-                <div class="dropdown-menu" id="profile-dropdown">
-                    <a href="{{ route('admin.admin_account') }}"><i class="bi bi-person-circle" id="account-icon"> </i>Account</a>
-                    <a href="#"><i class="bi bi-box-arrow-left" id="logout-icon"></i> Logout</a>
-                </div>
-            </div>   
-        </div>
-    </header>
+@section('title', 'Upload Document' )
 
-    <nav class="navbar">
-        <div class="navbar-content">
-            <div class="logo-container">
-                <img src="{{ asset ('images/sidebar-logo.png') }}" alt="Bicol University Logo" class="nav-logo">
-            </div>
-            <ul class="nav-icons">
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-house-fill" id="home-icon"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-grid-1x2-fill" id="dashboard-icon"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-file-earmark-fill" id="digitized-icon"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-people-fill" id="employees"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-hourglass-split" id="pendings"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-bell-fill" id="notification"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-cloud-arrow-up-fill" id="upload-icon"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-search" id="search-icon"></i></div></li>
-            </ul>
-            <div class="profile-settings">
-                <div class="profile-settings">
-                    <div class="icon-container" data-target="#sidebar"><i class="bi bi-door-open-fill"></i></div>
-                    <div class="icon-container" data-target="#sidebar"><img src="images/boy-1.png" alt="Profile Icon" class="profile-pic"></div>
-                </div>
-                
-            </div>
-        </div>
-    </nav>
+@section('custom-css')
+    <link rel="stylesheet" href="{{ asset ('css/upload_document.css') }}">
+@endsection
 
-    <div class="extra-sidebar" id="sidebar">
-        <div class="sidebar-content">
-            <div class="sidebar-title">
-                <h3>DASHBOARD</h3>
-                <i class="bi bi-text-right"></i>
-            </div>
-            <!--
-            <div class="search-container">
-                <input type="text" id="sidebar-search" placeholder="Search">
-                <i class="bi bi-search"></i>
-            </div>
-            -->
-            <ul>
-                <li><a href="{{ route ('home.admin') }}" id="home">Home</a></li>
+@section('main-id','dashboard-content')
 
-                <li><a href="{{ route ('admin.admin_dashboard') }}" id="report">Digitized Report</a></li>
-                <li>
-                    <a href="#" class="dropdown-toggle" id="digitized">Digitized Documents <i class="bi bi-chevron-right"></i></a>
-                    <ul class="more-dropdown-menu">
-                        <li><a href="admin_memorandum.html" id="memorandum"> Memorandum</a></li>
-                        <li><a href="admin_admin_orders.html" id="admin_order">Administrative Order</a></li>
-                        <li><a href="admin_mrsp.html" id="mrsp"> Monthly Report Service Personnel</a></li>
-                        <li><a href="admin_cms.html" id="cms"> Claim Monitoring Sheet</a></li>
-                        <li><a href="admin_audit.html" id="audit"> Audited Documents</a></li>
-                    </ul>
-                </li>
-            </ul>
-            
-            <ul>
-                <li>
-                    <a href="#" class="emp-dropdown-toggle" id="digitized">Employee <i class="bi bi-chevron-right"></i></a>
-                    <ul class="emp-dropdown-menu">
-                        <li><a href="admin_staffs.html" id="memorandum"> Office Staff</a></li>
-                        <li><a href="admin_deans.html" id="admin_order">College Dean</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#" class="pending-dropdown-toggle" id="digitized">Pendings <i class="bi bi-chevron-right"></i></a>
-                    <ul class="pending-dropdown">
-                        <li><a href="{{ route ('admin.documents.approved_docs') }}" id="approval">Approved</a></li>
-                        <li><a href="{{ route ('admin.documents.declined_docs') }}" id="decline">Declined</a></li>
-                        <li><a href="{{ route ('admin.documents.review_docs') }}" id="request">Review</a></li>
-                        <li><a href="{{ route ('admin.documents.request_docs') }}" id="request">Request</a></li>
-                    </ul>
-                </li>
-                <li><a href="admin_notification.html" id="announcements-icon"> Notifications</a></li>
-                <li><a href="admin_upload.html" id="upload">Upload</a></li>
-                <li><a href="admin_all_documents.html" id="search">Search</a></li>
-            </ul>
-            
-            <div class="profile-content">
-                <ul>
-                    <li><a href="sign-up.html"> Logout</a></li>
-                    <li><a href="admin_account.html" id="account">Profile</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-
-        <main id="dashboard-content">
+@section('content') 
             <section class="title">
                 <div class="title-content">
                     <h3>Upload Document</h3>
@@ -200,11 +91,12 @@
             </div>
         </div>
     </main>
-    
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
+@endsection
 
+ @section('custom-js')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
     <script src="{{ asset('js/upload_document.js') }}"></script>
-    <script src="{{ asset ('js/admin_page.js') }}"></script>
+@endsection
 
     <script>
     document.getElementById('uploadDocumentForm').addEventListener('submit', function(event) {
--- resources/views/admin/documents/request_docs.blade.php
+++ resources/views/admin/documents/request_docs.blade.php
@@ -1,118 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Request Document</title>
+@extends('layouts.admin_layout')
+
+@section('title', 'Request Document' )
+
+@section('custom-css')
     <link rel="stylesheet" href="{{ asset ('css/request.css') }}">
-    <link rel="stylesheet" href="{{ asset ('css/admin_page.css') }}">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-</head>
-<body>
-    <header>
-        <div class="header-content">
-            <div class="left-header">
-                <img src="{{ asset('images/Bicol_University.png') }}" alt="Bicol University Logo" class="logo">
-                <h1>BICOL <span>UNIVERSITY</span></h1>
-            </div>
-            <div class="search-container">
-                <input type="text" id="sidebar-search" placeholder="Search">
-                <i class="bi bi-search"></i>
-            </div>
-            <div class="profile-icon">
-                <img src="{{ asset ('images/user-circle-solid-24.png') }}" alt="Profile Icon" id="profile-icon">
-                <div class="dropdown-menu" id="profile-dropdown">
-                    <a href="head_account.html"><i class="bi bi-person-circle" id="account-icon"> </i>Account</a>
-                    <a href="#"><i class="bi bi-box-arrow-left" id="logout-icon"></i> Logout</a>
-                </div>
-            </div>   
-        </div>
-    </header>
+@endsection
 
-    <nav class="navbar">
-        <div class="navbar-content">
-            <div class="logo-container">
-                <img src="{{ asset ('images/sidebar-logo.png') }}" alt="Bicol University Logo" class="nav-logo">
-            </div>
-            <ul class="nav-icons">
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-house-fill" id="home-icon"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-grid-1x2-fill" id="dashboard-icon"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-file-earmark-fill" id="digitized-icon"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-people-fill" id="employees"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-hourglass-split" id="pendings"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-bell-fill" id="notification"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-cloud-arrow-up-fill" id="upload-icon"></i></div></li>
-                <li><div class="icon-container" data-target="#sidebar"><i class="bi bi-search" id="search-icon"></i></div></li>
-            </ul>
-            <div class="profile-settings">
-                <div class="profile-settings">
-                    <div class="icon-container" data-target="#sidebar"><i class="bi bi-door-open-fill"></i></div>
-                    <div class="icon-container" data-target="#sidebar"><img src="{{ asset ('images/boy-1.png') }}" alt="Profile Icon" class="profile-pic"></div>
-                </div>  
-            </div>
-        </div>
-    </nav>
+@section('main-id','dashboard-content')
 
-    <div class="extra-sidebar" id="sidebar">
-        <div class="sidebar-content">
-            <div class="sidebar-title">
-                <h3>DASHBOARD</h3>
-                <i class="bi bi-text-right"></i>
-            </div>
-                <!--
-                <div class="search-container">
-                    <input type="text" id="sidebar-search" placeholder="Search">
-                    <i class="bi bi-search"></i>
-                </div>
-                -->
-            <ul>
-                <li><a href="{{ route('home.admin') }}" id="home">Home</a></li>
-
-                <li><a href="{{ route('admin.admin_dashboard') }}" id="report">Digitized Report</a></li>
-                <li>
-                    <a href="#" class="dropdown-toggle" id="digitized">Digitized Documents <i class="bi bi-chevron-right"></i></a>
-                    <ul class="more-dropdown-menu">
-                        <li><a href="{{ route('admin.documents.memorandum') }}" id="memorandum"> Memorandum</a></li>
-                        <li><a href="admin_admin_orders.html" id="admin_order">Administrative Order</a></li>
-                        <li><a href="admin_mrsp.html" id="mrsp"> Monthly Report Service Personnel</a></li>
-                        <li><a href="admin_cms.html" id="cms"> Claim Monitoring Sheet</a></li>
-                        <li><a href="admin_audit.html" id="audit"> Audited Documents</a></li>
-                    </ul>
-                </li>
-            </ul>
-                
-            <ul>
-                <li>
-                    <a href="#" class="emp-dropdown-toggle" id="digitized">Employee <i class="bi bi-chevron-right"></i></a>
-                        <ul class="emp-dropdown-menu">
-                            <li><a href="{{ route ('admin.office_staff') }}" id="memorandum"> Office Staff</a></li>
-                            <li><a href="{{ route ('admin.college_dean') }}" id="admin_order">College Dean</a></li>
-                        </ul>
-                </li>
-                <li>
-                    <a href="#" class="pending-dropdown-toggle" id="digitized">Pendings <i class="bi bi-chevron-right"></i></a>
-                    <ul class="pending-dropdown">
-                        <li><a href="{{ route ('admin.documents.approved_docs') }}" id="approval">Approved</a></li>
-                        <li><a href="{{ route ('admin.documents.declined_docs') }}" id="decline">Declined</a></li>
-                        <li><a href="{{ route ('admin.documents.review_docs') }}" id="request">Review</a></li>
-                        <li><a href="{{ route ('admin.documents.request_docs') }}" id="request">Request</a></li>
-                    </ul>
-                </li>
-                <li><a href="admin_notification.html" id="announcements-icon"> Notifications</a></li>
-                <li><a href="{{ route('admin.admin_upload_document') }}" id="upload">Upload</a></li>
-                <li><a href="{{ route('admin.documents.all_docs') }}" id="search">Search</a></li>
-            </ul>
-                <div class="profile-content">
-                    <ul>
-                        <li><a href="{{ route('logout') }}"> Logout</a></li>
-                        <li><a href="{{route('admin.admin_account')}}" id="account">Profile</a></li>
-                    </ul>
-                </div>
-            </div>
-    </div>
-
-    <main id="dashboard-content">
+@section('content') 
         <section class="title">
             <div class="title-content">
                 <h3>Requested Documents</h3>
@@ -205,16 +101,11 @@
             </div>
         </div>
     </main>
+@endsection
 
-    <footer>
-        <div class="footer-content">
-            <p>&copy; DATOS 2024 Bicol University. All Rights Reserved.</p>
-            <p>Contact us: <a href="mailto:datos.bu@gmail.com">datos.bu@gmail.com</a></p>
-        </div>
-    </footer>
-
-    <script src="{{ asset ('js/request.js') }}"></script>
-    <script src="{{ asset ('js/admin_page.js') }}"></script>
+@section('custom-js')
+    <script src="{{ asset('js/request.js') }}"></script>
+@endsection
 
 </body>
 </html>
